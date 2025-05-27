762523dae2d7c54388bbe33e735cdd96c96e5ff1
Justus <justus.d2025@gmail.com>
2025-05-27 13:02:07 +0200

    a0.3

--- titanic.py
+++ titanic.py
@@ -1,5 +1,7 @@
 from load_data import load_data
-from json import load as jsload
+from json import load as jsload, dumps as jssave
+from time import perf_counter
+
 
 class Titanic:
     def __init__(self):
@@ -7,16 +9,28 @@ class Titanic:
         self.is_running = True
     def run(self):
         print(self.data.keys())
-        
+        with open('test.json','w') as f:
+            f.write(jssave(self.data,indent=4))
+        self.data['data']
         while self.is_running:
             print('Welcome to the Ships CLI! Enter \'help\' to view available commands.')
             user_input = input('>>> ')
+            
+            self.check_command(user_input) 
     def check_command(self,inp: str) -> None:
-        match inp:
-            case 'help':
-                print()
-            
-            
+        if inp == 'help':
+                print("help\nshow_countries\ntop_countries\n")
+        elif inp == 'show_countries': 
+
+                countrys = {ship['COUNTRY'] for ship in self.data['data']} #store all countrys in a set to eliminate duplicates
+
+                print_string = '\n'.join(countrys) # create a string that contains all countrys. 
+                # [REASON]: Calling the print function multiple times is very slow.
+                # print_string = 1.5ms (^3.2 times faster) + time for creating the string approximately (^3 times)
+                # print for    = 4.8ms
+                print(print_string)
+        elif inp == 'top_countries':
+                pass
     def load_data(file_path: str) -> dict:
         "Load a json file"
         with open(file_path, "r") as handle:
